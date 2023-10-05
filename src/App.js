fdc5f40464301f185e95ec5fba0f9e8ee3f96745
taehyun <dbxogus2080@naver.com>
2023-10-06 04:03:33 +0900

    유태현 1주차_과제

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import './App.css';
+import { RouterProvider, createBrowserRouter, BrowserRouter as Router } from "react-router-dom";
+import Home from "./pages/Home";
+
+function App() {
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  }
+]);
+
+  return (
+    <RouterProvider router={router}>
+     </RouterProvider>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = ()=>{
+    return (
+     <>
+        <div>
+           본인 이름 넣기
+        </div>
+    </>
+    )
+}
+
+export default Home;
